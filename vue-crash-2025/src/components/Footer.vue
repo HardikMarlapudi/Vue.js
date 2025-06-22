db97e7dc5d5155751878837b5d22eb52fd023828
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-06-22 02:23:15 -0400

    working on Single Task Component

---
+++ vue-crash-2025/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>&copy; 2025 Hardik Marlapudi. All Rights Reserved.</footer>
+</template>
+
+<script>
+import Footer from './Footer.vue'
+
+export default {
+    components: {
+        Footer
+    }
+}
+
+</script>
+
+
+<style>
+footer {
+    display: block;
+    text-align: center;
+    padding: 350px 10px;
+    position: relative;
+}
+</style>
